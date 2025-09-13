984f58fd1a243fced3bfd6da79e4fdf3672e3c48
DHRUV-SAVE21 <dhruvsave2311@gmail.com>
2025-09-13 20:15:03 +0530

    Create 876.py

---
+++ 876.py
@@ -0,0 +1,10 @@
+# 876. Middle of the Linked List
+class Solution(object):
+    def middleNode(self, head):
+        slow,fast=head,head
+
+        while fast and fast.next:
+            slow=slow.next
+            fast=fast.next.next
+
+        return slow
