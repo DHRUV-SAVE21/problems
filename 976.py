8862b5294966c0aa32092b153991cfe10c614118
DHRUV-SAVE21 <dhruvsave2311@gmail.com>
2025-09-28 19:43:51 +0530

    Create 976.py

---
+++ 976.py
@@ -0,0 +1,10 @@
+# 976. Largest Perimeter Triangle
+
+class Solution(object):
+    def largestPerimeter(self, nums):
+        nums.sort(reverse=True)
+        for i in range(len(nums)-2):
+            a,b,c=nums[i],nums[i+1],nums[i+2]
+            if a<b+c:
+                return a+b+c
+        return 0
